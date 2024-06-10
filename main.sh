2163c25b51102032911a02620e02a9e492511145
Mohammed Imthathullah <imthath.m@hotmail.com>
2024-06-10 10:52:21 +0530

    remove verbose options from commands

--- main.sh
+++ main.sh
@@ -4,7 +4,7 @@
 CURRENT_FOLDER="$PWD"
 DERIVED_DATA_PATH="$HOME/Library/Developer/Xcode/DerivedData"
 REPO_NAME="grid-game"
-REPO_PATH="$HOME/skydevz/learn-ci/$REPO_NAME" # Update this to your actual repo path
+REPO_PATH="$HOME/skydevz/learn-ci/$REPO_NAME"
 CACHE_FOLDER="dd-tar-cache"
 CACHE_FILE="dd.tar"
 TIME_RESTORE_SCRIPT="git-restore-mtime.py"
@@ -37,7 +37,7 @@ xcodebuild -project 'GridGame.xcodeproj' \
 -sdk 'iphonesimulator' \
 -destination 'platform=iOS Simulator,OS=17.2,name=iPhone 15' \
 -showBuildTimingSummary \
-build
+build | sed -n -e '/Build Timing Summary/,$p'
 
 # # Cache Derived Data
 echo "Caching Derived Data..."
@@ -58,19 +58,15 @@ cd ..
 git clone -b feature https://github.com/imthath-m/grid-game.git
 cd $REPO_PATH
 
-# Update last modified time based on git
-echo "Updating last modified time for feature branch..."
-git ls-files | xargs touch
+# Restore DD based on cache
+cd $CURRENT_FOLDER
+tar xPpf $CACHE_FOLDER/$CACHE_FILE
 
 # Update last modified time based on git
 echo "Updating last modified time..."
 cp $CURRENT_FOLDER/$TIME_RESTORE_SCRIPT $REPO_PATH/$TIME_RESTORE_SCRIPT
 cd $REPO_PATH
 python3 $TIME_RESTORE_SCRIPT
-
-# Set DD based on cache
-cd $CURRENT_FOLDER
-tar xvPpf $CACHE_FOLDER/$CACHE_FILE
 
 # Run `xcodebuild` with `-showBuildTimingSummary` option for the feature branch
 echo "Running xcodebuild for feature branch..."
@@ -80,4 +76,4 @@ xcodebuild -project 'GridGame.xcodeproj' \
 -sdk 'iphonesimulator' \
 -destination 'platform=iOS Simulator,OS=17.2,name=iPhone 15' \
 -showBuildTimingSummary \
-build
+build | sed -n -e '/Build Timing Summary/,$p'
